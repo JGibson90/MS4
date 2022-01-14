51c73dfb2dcb792d71fdd14d4c0806b108767fdf
JGibson90 <j_gibson6@hotmail.com>
2022-01-14 11:53:49 +0000

    added date to blogs model and template

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Blog(models.Model):
     blog_title = models.CharField(max_length=80, null=True, blank=True)
     blog_body = models.TextField(null=True, blank=True, default="")
     blog_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_date = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.blog_title
